752a9facc1b39aeb7119ac962d2518578fb2c645
raghad0bamansour <66147521+raghad0bamansour@users.noreply.github.com>
2023-08-15 20:39:39 +0000

    Correct 2

--- utils.py
+++ utils.py
@@ -6,5 +6,12 @@ clf = pickle.load(open("models/clf.pkl", "rb"))
 def make_prediction(review_text):
     tokenized_review = cv.transform([review_text])
     prediction = clf.predict(tokenized_review)
+    if prediction == 1:
+        prediction = 1
+    elif prediction == 2:
+        prediction = 1
+    else:
+        prediction = 0
+    
     #prediction = 1 if prediction == 1 else -1
     return prediction
