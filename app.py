2ea3c7374ecb8b7ec97181cee49528bcb8027bf1
raghad0bamansour <66147521+raghad0bamansour@users.noreply.github.com>
2023-08-15 19:07:26 +0000

    added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 cv = pickle.load(open("models/cv.pkl", "rb"))
@@ -18,6 +18,16 @@ def predict():
     prediction = clf.predict(tokenized_review)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html",prediction=prediction, review_text=review_text)
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json(force=True)  # Get data posted as a json
+    review_text = data['content']
+    tokenized_review = cv.transform([review_text]) # X 
+    prediction = clf.predict(tokenized_review)
+    # If the email is spam prediction should be 1
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction': prediction, 'review_text': review_text})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
